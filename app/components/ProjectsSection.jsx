c719efe27badb48bd6c0a2a09824facd9f878003
Sahil Singh <200104050@hbtu.ac.in>
2024-03-05 20:21:05 +0530

    commit

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -12,7 +12,7 @@ const projectsData = [
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
     gitUrl: "",
-    previewUrl: "/",
+    previewUrl: "https://portfolio-sahil259.netlify.app/",
   },
   {
     id: 2,
