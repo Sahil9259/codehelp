95595eb6c7c879807d3f569974d894dae63eeed6
Sahil Singh <200104050@hbtu.ac.in>
2024-03-06 20:05:57 +0530

    next.config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev, isServer }) => {
+
+      config.optimization.minimize = false;
+
+    return config;
+  },
+}
 
 export default nextConfig;
